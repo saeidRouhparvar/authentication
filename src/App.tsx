7e06bd80d78d09846712d2bf62303acd1f57823b
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-22 15:16:49 +0330

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraBaseProvider, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraBaseProvider>
+      <Box>asasa</Box>
+    </ChakraBaseProvider>
+  );
+}
+
+export default App;
