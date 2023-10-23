61c47ed87beef35f576115bb39eb885689471589
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-23 23:19:07 +0330

    create wellcome component

---
+++ src/pages/login/Wellcome.tsx
@@ -0,0 +1,9 @@
+import { Box, Flex } from "@chakra-ui/react"
+import { MainLogo } from "../../assets/Svg"
+
+const Welcome = () => {
+    return (
+        <Flex justify={"center"} mt={"50vh"}>{MainLogo}</Flex>
+    )
+}
+export default Welcome
