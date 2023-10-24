cb11b2d1a2a19d1865120f9d0a70ef9286c4ae1f
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-24 23:48:12 +0330

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { ChakraBaseProvider, Box } from "@chakra-ui/react";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import { useRoutes } from "react-router-dom";
+import Welcome from "./pages/login/Wellcome";
+import OtpVerification from "./pages/login/OtpVerification";
+import OtpCode from "./pages/login/OtpCode";
+
 
 function App() {
+ 
+  const routes = useRoutes([
+    {path:'/',element: <Welcome/>},
+    {path:'/otp',element: <OtpVerification/>},
+    {path:'/otp_code',element: <OtpCode/>},
+
+  ])
   return (
     <ChakraBaseProvider>
-      <Box>asasa</Box>
+      {routes}
     </ChakraBaseProvider>
   );
 }
