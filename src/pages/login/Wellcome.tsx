c0108a7e73b96bdf3bcb802ebbab181351329a1b
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-25 09:24:59 +0330

    add timeout to change

--- src/pages/login/Wellcome.tsx
+++ src/pages/login/Wellcome.tsx
@@ -1,7 +1,15 @@
 import { Box, Flex } from "@chakra-ui/react"
 import { MainLogo } from "../../assets/Svg"
+import {useNavigate} from 'react-router-dom'
 
 const Welcome = () => {
+
+    const navigate = useNavigate()
+
+    setTimeout(() => {
+        navigate('/otp')
+    },3000)
+
     return (
         <Flex justify={"center"} mt={"50vh"}>{MainLogo}</Flex>
     )
